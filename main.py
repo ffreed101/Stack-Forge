46e1a5ecf02eb6bc5e7b1c8377c1ff67393ae1b5
Falon Freed <165129172+ffreed101@users.noreply.github.com>
2024-11-07 00:54:33 -0600

    Created new display and input functions

--- main.py
+++ main.py
@@ -1,3 +1,61 @@
+def displayGreeting():
+    print("Welcome to Stack Forge!")
+
+def displayMenu(menuTitle, menuOptions):
+
+    print(f"\n--- {menuTitle} ---")
+
+    for i in range(len(menuOptions)):
+        if menuOptions[i] == menuOptions[-1]:
+            print(f"0) {menuOptions[i]}")
+        else:
+            print(f"{i+1}) {menuOptions[i]}")
+
+def getAndValidateInput(choices):
+    # Makes the little choice guide that goes in parentheses using all available choices
+    choiceGuideText = "/".join(choices)
+
+    # Get Input
+    userChoice = input(f"Enter your choice({choiceGuideText}): ")
+
+    # Validate Input
+    while userChoice not in choices:
+        print("\nInvalid Selection. Please try again. \n")
+        userChoice = input(f"Enter your choice({choiceGuideText}): ")
+
+    return userChoice
 
 
 def main():
+
+    # Menu Lists. Might Dictionary this later.
+    mainMenuOptions = ["Create Item", "Query Item", "View Items", "Edit Item", "Delete Item", "Exit"]
+    mainMenuChoices = ["1", "2", "3", "4", "5", "0"]
+
+    displayGreeting()
+
+    while True:
+        displayMenu("Main Menu", mainMenuOptions)
+        match getAndValidateInput(mainMenuChoices):
+            case "1":
+                # Implement create item
+                pass
+            case "2":
+                # Implement query item
+                pass
+            case "3":
+                # Implement view items
+                pass
+            case "4":
+                # Implement edit item
+                pass
+            case "5":
+                # Implement delete item
+                pass
+            case "0":
+                print("\nClosing...")
+                break
+            case _:
+                print("\nERROR: Validation Issue. Restarting menu...")
+
+main()
